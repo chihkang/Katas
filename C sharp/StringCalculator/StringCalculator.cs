867df3266c082cea547858a041300a4c5d053cd5
chihkang <chihkang@me.com>
2015-07-12 22:17:29 +0800

    StringCalculator

---
+++ C sharp/StringCalculator/StringCalculator.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingGameTest.StringCalculator
+{
+    class StringCalculator
+    {
+        private int Sum;
+
+        private char[] defaultDelimter = new char[3];
+
+        public int Add(string number) 
+        {
+            defaultDelimterSetting();
+
+            return string.IsNullOrEmpty(number) ? 0 : GetSum(number);
+        }
+
+        private int GetSum(string number) 
+        {
+            return isParseInt(number) ? VelidateNumbersArePerRule(Sum) : SplitDelimter_GetSum(number);
+        }
+
+        private bool isParseInt(string number) 
+        {
+            return Int32.TryParse(number,out Sum);
+        }
+
+        private int SplitDelimter_GetSum(string number) 
+        {   
+            int temp=0;
+
+            if(HasSpecificDelimter(number))
+            {   
+                //start with //
+                var new_delimter = GetSpecificDelimter(number);
+
+                addDelimter(new_delimter);
+            }
+
+            string[] numbers = number.Split(defaultDelimter);
+
+            foreach (var item in numbers)
+            {
+                if (isParseInt(item)) temp += GreaterThan1000(Sum);
+            }
+ 
+            return temp;
+        }
+
+        private bool HasSpecificDelimter(string number)
+        {
+            return number.StartsWith("//");
+        }
+        private char[] getDelimter() 
+        {   
+            return defaultDelimter;
+        }
+
+        private void addDelimter(char[] delimter) 
+        {
+            defaultDelimter[2] = delimter[0];
+        }
+
+        private char[] GetSpecificDelimter(string number)
+        {
+            return number.Substring(2, number.IndexOf("\n") - 2).ToCharArray();
+        }
+        private void defaultDelimterSetting()
+        {
+            defaultDelimter[0] = '\n';
+            defaultDelimter[1] = ',';
+        }
+        private int VelidateNumbersArePerRule(int number) 
+        {
+            if (number < 0)
+            {
+                throw new ArgumentException(string.Format("string contains [{0}], which does not meet rule. entered number should not negative.", number));
+            }
+            else 
+            {
+                return GreaterThan1000(number);
+            }
+        }
+        private int GreaterThan1000(int number) 
+        {
+            return number > 1000 ? 0 : number;
+        }
+    }
+}
---
+++ C sharp/StringCalculator/StringCalculatorTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BowlingGameTest.StringCalculator
+{
+    [TestClass]
+    public class StringCalculatorTest
+    {
+        private StringCalculator sc;
+
+        [TestInitialize]
+        public void setUp()
+        {
+            sc = new StringCalculator();
+        }
+        [TestMethod]
+        public void Test_Add_Empty()
+        {     
+            Assert.AreEqual(0,sc.Add(""));
+        }
+        [TestMethod]
+        public void Test_Add_One_Number() 
+        {
+            int n=20;
+            Assert.AreEqual(n,sc.Add(n+""));
+        }
+        [TestMethod]
+        public void Test_Add_Two_Number() 
+        {
+            Assert.AreEqual(6, sc.Add("1\n2,3"));
+        }
+        [TestMethod]
+        public void Test_Add_Other() 
+        {
+            Assert.AreEqual(3, sc.Add("//;\n1;2"));
+        }
+        [TestMethod]
+        public void Test_Negative_Number() 
+        {
+            //Assert.AreNotEqual(-1, sc.Add("-1"));
+            //sc.Add("-1");
+        }
+        [TestMethod]
+        public void Test_Greater_1000_Number() 
+        {
+            Assert.AreEqual(0, sc.Add("\\;1001,0\n2000"));
+        }
+    }
+}
